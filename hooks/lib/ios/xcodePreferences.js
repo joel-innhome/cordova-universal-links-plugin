68cf4a47e4c286f5affdbd9a70d97a1ed5263268
Joel Arnott <joel-innhome@j-factor.com>
2026-01-03 13:52:56 +1000

    Replaced requireCordovaModule usage for NPM modules with standard require

--- hooks/lib/ios/xcodePreferences.js
+++ hooks/lib/ios/xcodePreferences.js
@@ -8,6 +8,10 @@ Which is:
 
 var path = require('path');
 var compare = require('node-version-compare');
+var glob = require('glob');
+var fs = require('fs');
+var shelljs = require('shelljs');
+var xcode = require('xcode');
 var ConfigXmlHelper = require('../configXmlHelper.js');
 var IOS_DEPLOYMENT_TARGET = '8.0';
 var COMMENT_KEY = /_comment$/;
@@ -147,7 +151,7 @@ function loadProjectFile() {
       projectFile = platform_ios.parseProjectFile(iosPlatformPath());
     } catch (e) {
       // Then cordova 7.0
-      var project_files = context.requireCordovaModule('glob').sync(path.join(iosPlatformPath(), '*.xcodeproj', 'project.pbxproj'));
+      var project_files = glob.sync(path.join(iosPlatformPath(), '*.xcodeproj', 'project.pbxproj'));
       
       if (project_files.length === 0) {
         throw new Error('does not appear to be an xcode project (no xcode project file)');
@@ -155,14 +159,12 @@ function loadProjectFile() {
       
       var pbxPath = project_files[0];
       
-      var xcodeproj = context.requireCordovaModule('xcode').project(pbxPath);
+      var xcodeproj = xcode.project(pbxPath);
       xcodeproj.parseSync();
       
       projectFile = {
         'xcode': xcodeproj,
         write: function () {
-          var fs = context.requireCordovaModule('fs');
-            
           var frameworks_file = path.join(iosPlatformPath(), 'frameworks.json');
           var frameworks = {};
           try {
@@ -172,7 +174,7 @@ function loadProjectFile() {
           fs.writeFileSync(pbxPath, xcodeproj.writeSync());
           if (Object.keys(frameworks).length === 0){
             // If there is no framework references remain in the project, just remove this file
-            context.requireCordovaModule('shelljs').rm('-rf', frameworks_file);
+            shelljs.rm('-rf', frameworks_file);
             return;
           }
           fs.writeFileSync(frameworks_file, JSON.stringify(this.frameworks, null, 4));
